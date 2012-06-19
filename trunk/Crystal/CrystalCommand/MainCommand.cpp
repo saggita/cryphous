a8f177de7a2af3dbfa9aabd48efbfd3ad2f661c9
cryphous@gmail.com <cryphous@gmail.com@c4768a9b-8396-9c1e-e381-6b4412480993>
2012-06-19 12:10:07 +0000

    MainCommandBase を　MainCommand にマージ．

--- trunk/Crystal/CrystalCommand/MainCommand.cpp
+++ trunk/Crystal/CrystalCommand/MainCommand.cpp
@@ -8,14 +8,19 @@
 #include "ParticleInfoCommand.h"
 #include "SimulationCommand.h"
 #include "ParticleExportCommand.h"
+#include "GraphicsSettingCommand.h"
+#include "ProfileInfoCommand.h"
+#include "ApplicationSettings.h"
 
 #include "../CrystalPhysics/PhysicsObjectFactory.h"
 #include "../CrystalPhysics/Simulation.h"
 #include "../CrystalPhysics/ParticleFactory.h"
 
-#include <map>
+#include "../CrystalGraphics/Renderer.h"
+#include "../CrystalGraphics/GraphicsSettings.h"
 
 using namespace Crystal::Physics;
+using namespace Crystal::Graphics;
 using namespace Crystal::Command;
 
 using namespace System;
@@ -27,7 +32,6 @@ using namespace System::Diagnostics;
 
 
 MainCommand::MainCommand(System::Windows::Forms::PictureBox^ pictureBox)
-: MainCommandBase( pictureBox )
 {
 	this->simulationSettingCommand = gcnew SimulationSettingCommand();
 	this->objectSettingCommand = gcnew ObjectSettingCommand();
@@ -37,6 +41,12 @@ MainCommand::MainCommand(System::Windows::Forms::PictureBox^ pictureBox)
 	this->animationCommandBase = gcnew SimulationCommand();
 	this->particleExportSettingCommand = gcnew ExportSettingCommand("particles", "cparticles");
 	this->particleExportCommand = gcnew ParticleExportCommand( particleExportSettingCommand );
+	this->graphicsSettingCommand = gcnew GraphicsSettingCommand();
+	this->fileIOCommand = gcnew FileIOCommand();
+	this->profileInfoCommand = gcnew ProfileInfoCommand();
+	this->pictureBox = pictureBox;
+
+	ApplicationSettings::get()->renderer->setPictureBox( (HWND)(pictureBox->Handle.ToInt32()) );
 }
 
 MainCommand::~MainCommand(void)
@@ -45,6 +55,59 @@ MainCommand::~MainCommand(void)
 
 void MainCommand::proceedSimulation()
 {
-	MainCommandBase::proceedSimulation();
+	animationCommandBase->proceed();
+	rendering();
 	particleExportCommand->exportToFile( animationCommandBase->getStep() );
+}
+
+void MainCommand::refreshSimulation()
+{
+	animationCommandBase->refresh();
+	rendering();
+}
+
+void MainCommand::rendering()
+{
+	ApplicationSettings::get()->renderer->rendering( ApplicationSettings::get()->factory, pictureBox->Width, pictureBox->Height, ApplicationSettings::get()->simulationSetting->boundaryBox );
+}
+
+void MainCommand::displayInformation(System::Windows::Forms::ListBox^ listBox){
+	profileInfoCommand->display( listBox );
+}
+
+void MainCommand::rotateX(int angle)
+{
+	GraphicsSettings::get()->angleX += angle;
+	rendering();
+}
+
+void MainCommand::rotateY(int angle)
+{
+	GraphicsSettings::get()->angleY += angle;
+	rendering();
+}
+
+void MainCommand::rotateZ(int angle)
+{
+	GraphicsSettings::get()->angleZ += angle;
+	rendering();
+}
+
+void MainCommand::zoom(double zoom)
+{
+	GraphicsSettings::get()->zoom += zoom;
+	rendering();
+}
+
+void MainCommand::move(double x, double y)
+{
+	GraphicsSettings::get()->cameraX += x;
+	GraphicsSettings::get()->cameraY += y;
+	rendering();
+}
+
+void MainCommand::viewReset()
+{
+	GraphicsSettings::get()->resetCameraAndAngle();
+	rendering();
 }
