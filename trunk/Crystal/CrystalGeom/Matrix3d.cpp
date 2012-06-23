fd28f999d8a986ddba54690191f39fcd5c871b97
cryphous@gmail.com <cryphous@gmail.com@c4768a9b-8396-9c1e-e381-6b4412480993>
2012-06-23 08:15:51 +0000

    幾何ライブラリをヘッダファイルのみの実装に移行． ただしBoxのみVector3dとの循環依存により.cppが残ったままになっている． 変形専用のクラスを作ることで対処を予定．

--- trunk/Crystal/CrystalGeom/Matrix3d.cpp
+++ trunk/Crystal/CrystalGeom/Matrix3d.cpp
@@ -7,179 +7,35 @@
 
 using namespace Crystal::Geom;
 
-Matrix3d::Matrix3d(void) :
-x00( 0.0), x01( 0.0), x02( 0.0),
-x10( 0.0), x11( 0.0), x12( 0.0),
-x20( 0.0), x21( 0.0), x22( 0.0)
-{
-}
 
-Matrix3d::Matrix3d(const double x00, const double x01, const double x02, 
-				   const double x10, const double x11, const double x12, 
-				   const double x20, const double x21, const double x22) :
-x00( x00), x01( x01), x02( x02),
-x10( x10), x11( x11), x12( x12),
-x20( x20), x21( x21), x22( x22)
-{
-}
+Matrix3d Matrix3d::product(const Matrix3d& rhs) {
+		Vector3d lhs0( x00, x01, x02 );
+		Vector3d lhs1( x10, x11, x12 );
+		Vector3d lhs2( x20, x21, x22 );
 
-void Matrix3d::setZero()
-{
-	x00 = x01 = x02 = 0.0;
-	x10 = x11 = x12 = 0.0;
-	x20 = x21 = x22 = 0.0;
-	assert( isZero() );
-}
+		Vector3d rhs0( rhs.x00, rhs.x10, rhs.x20 );
+		Vector3d rhs1( rhs.x01, rhs.x11, rhs.x21 );
+		Vector3d rhs2( rhs.x02, rhs.x12, rhs.x22 );
 
-void Matrix3d::setIdentity()
-{
-	x00 = 1.0; x01 = 0.0; x02 = 0.0;
-	x10 = 0.0; x11 = 1.0; x12 = 0.0;
-	x20 = 0.0; x21 = 0.0; x22 = 1.0;
-	assert( isIdentity() );
-}
+		x00 = lhs0.getInnerProduct( rhs0 );
+		x01 = lhs0.getInnerProduct( rhs1 );
+		x02 = lhs0.getInnerProduct( rhs2 );
 
-bool Matrix3d::isZero() const
-{
-	Matrix3d zeroMatrix;
-	return this->equals( zeroMatrix );
-}
+		x10 = lhs1.getInnerProduct( rhs0 );
+		x11 = lhs1.getInnerProduct( rhs1 );
+		x12 = lhs1.getInnerProduct( rhs2 );
 
-bool Matrix3d::isIdentity() const
-{
-	Matrix3d identityMatrix( 1.0, 0.0, 0.0,
-							0.0, 1.0, 0.0,
-							0.0, 0.0, 1.0);
-	return this->equals( identityMatrix );
-}
+		x20 = lhs2.getInnerProduct( rhs0 );
+		x21 = lhs2.getInnerProduct( rhs1 );
+		x22 = lhs2.getInnerProduct( rhs2 );
 
-bool Matrix3d::equals( const Matrix3d& rhs ) const
-{
-	return
-		Tolerances::isEqualAsDenominator( x00, rhs.x00 ) &&
-		Tolerances::isEqualAsDenominator( x01, rhs.x01 ) &&
-		Tolerances::isEqualAsDenominator( x02, rhs.x02 ) &&
-		Tolerances::isEqualAsDenominator( x10, rhs.x10 ) &&
-		Tolerances::isEqualAsDenominator( x11, rhs.x11 ) &&
-		Tolerances::isEqualAsDenominator( x12, rhs.x12 ) &&
-		Tolerances::isEqualAsDenominator( x20, rhs.x20 ) &&
-		Tolerances::isEqualAsDenominator( x21, rhs.x21 ) &&
-		Tolerances::isEqualAsDenominator( x22, rhs.x22 );
-}
-
-double Matrix3d::getDeterminant() const
-{
-	return x00 * x11 * x22
-		+ x02 * x10 * x21
-		+ x20 * x01 * x12
-		- x02 * x11 * x20
-		- x00 * x12 * x21
-		- x01 * x10 * x22;
-}
-
-Matrix3d Matrix3d::getInverse() const
-{
-	const double denominator = getDeterminant();
-	assert( !Tolerances::isEqualAsDenominator( denominator ) );
-	
-	const double i00 = x11 * x22 - x12 * x21;
-	const double i01 = x21 * x02 - x22 * x01;
-	const double i02 = x01 * x12 - x02 * x11;
-
-	const double i10 = x12 * x20 - x10 * x22;
-	const double i11 = x22 * x00 - x20 * x02;
-	const double i12 = x02 * x10 - x00 * x12;
-
-	const double i20 = x10 * x21 - x11 * x20;
-	const double i21 = x20 * x01 - x21 * x00;
-	const double i22 = x00 * x11 - x01 * x10;
-
-	Matrix3d matrix( i00, i01, i02,
-		i10, i11, i12,
-		i20, i21, i22 );
-	matrix.scale( 1.0 / denominator );
-	return matrix;
-}
-
-Matrix3d Matrix3d::add(const Matrix3d& rhs)
-{
-	x00 += rhs.x00;
-	x01 += rhs.x01;
-	x02 += rhs.x02;
-
-	x10 += rhs.x10;
-	x11 += rhs.x11;
-	x12 += rhs.x12;
-
-	x20 += rhs.x20;
-	x21 += rhs.x21;
-	x22 += rhs.x22;
-
-	return *this;
-}
-
-Matrix3d Matrix3d::getAdd(const Matrix3d& rhs) const
-{
-	Matrix3d matrix = *this;
-	return matrix.add( rhs);
-}
-
-Matrix3d Matrix3d::product(const Matrix3d& rhs)
-{
-	Vector3d lhs0( x00, x01, x02 );
-	Vector3d lhs1( x10, x11, x12 );
-	Vector3d lhs2( x20, x21, x22 );
-
-	Vector3d rhs0( rhs.x00, rhs.x10, rhs.x20 );
-	Vector3d rhs1( rhs.x01, rhs.x11, rhs.x21 );
-	Vector3d rhs2( rhs.x02, rhs.x12, rhs.x22 );
-
-	x00 = lhs0.getInnerProduct( rhs0 );
-	x01 = lhs0.getInnerProduct( rhs1 );
-	x02 = lhs0.getInnerProduct( rhs2 );
-
-	x10 = lhs1.getInnerProduct( rhs0 );
-	x11 = lhs1.getInnerProduct( rhs1 );
-	x12 = lhs1.getInnerProduct( rhs2 );
-
-	x20 = lhs2.getInnerProduct( rhs0 );
-	x21 = lhs2.getInnerProduct( rhs1 );
-	x22 = lhs2.getInnerProduct( rhs2 );
-
-	return *this;
+		return *this;
 }
 
 Matrix3d Matrix3d::getProduct(const Matrix3d& rhs) const
 {
-	Matrix3d matrix = *this;
-	return matrix.product( rhs);
-}
-
-void Matrix3d::setRotateX(const double angle)
-{
-	*(this) = Matrix3d(
-		1.0, 0.0, 0.0,
-		0.0, cos(angle), -sin(angle),
-		0.0, sin(angle), cos(angle )
-		);
-}
-
-void Matrix3d::setRotateY(const double angle)
-{
-	*(this) = Matrix3d(
-		cos(angle), 0.0, sin(angle),
-		0.0, 1.0, 0.0,
-		-sin(angle), 0.0, cos(angle)
-		);
-}
-
-void Matrix3d::setRotateZ(const double angle)
-{
-	*(this) = Matrix3d(
-		cos(angle), -sin(angle), 0.0,
-		sin(angle), cos(angle), 0.0,
-		0.0, 0.0, 1.0
-		);
+		Matrix3d matrix = *this;
+		return matrix.product( rhs);
 }
 
 Matrix3d Matrix3d::scale(const double factor)
--- trunk/Crystal/CrystalGeom/Quaternion.h
+++ trunk/Crystal/CrystalGeom/Quaternion.h
@@ -1,27 +1,59 @@
 #ifndef __QUATERNION_H__
 #define __QUATERNION_H__
 
+#include "Vector3d.h"
+
 namespace Crystal{
 	namespace Geom{
 
-class Vector3d;
+
 class Matrix3d;
 
 class Quaternion
 {
 public:
+	
+	Quaternion(void) : x( 0.0), y( 0.0), z( 0.0), w( 1.0) 
+	{}
+	
+	Quaternion::Quaternion(const double x, const double y, const double z, const double w) : x( x), y( y), z( z), w( w)
+	{}
+	
+	Quaternion::Quaternion(const Vector3d& axis, const double angle) :
+	x( axis.getX() * sin(angle * 0.5) ), 
+		y( axis.getY() * sin(angle * 0.5) ), 
+		z( axis.getZ() * sin(angle * 0.5) ), 
+		w( cos(angle * 0.5) )
+	{
+		assert( axis.isNormalized() );
+	}
 
-	Quaternion(void);
+	Matrix3d getMatrix() const {
+		const double x00 = ( 1.0 - 2.0 * y * y - 2.0 * z * z );
+		const double x01 = ( 2.0 * x * y - 2.0 * z * w );
+		const double x02 = ( 2.0 * x * z + 2.0 * y * w );
 
-	Quaternion(const double x, const double y, const double z, const double w);
+		const double x10 = ( 2.0 * x * y + 2.0 * z * w );
+		const double x11 = ( 1.0 - 2.0 * x * x - 2.0 * z * z );
+		const double x12 = ( 2.0 * y * z - 2.0 * x * w );
+	
+		const double x20 = ( 2.0 * x * z - 2.0 * y * w );
+		const double x21 = ( 2.0 * y * z + 2.0 * x * w );
+		const double x22 = ( 1.0 - 2.0 * x * x - 2.0 * y * y);
 
-	Quaternion(const Vector3d& axis, const double angle);
-
-	Matrix3d getMatrix() const;
-
-	Quaternion& operator+=( const Quaternion& rhs );
-
-	//Quaternion& operator*=( const Quaternion& rhs );
+		return Matrix3d( x00, x01, x02,
+							x10, x11, x12,
+							x20, x21, x22 );
+	}
+	
+	Quaternion& operator+=( const Quaternion& rhs )
+	{
+		x += rhs.getX();
+		y += rhs.getY();
+		z += rhs.getZ();
+		w += rhs.getW();
+		return *(this);
+	}
 
 	double getX() const { return x; }
 
@@ -31,11 +63,22 @@ public:
 
 	double getW() const { return w; }
 
-	double getNorm() const;
+	double getNorm() const {
+		return sqrt( pow( x, 2 ) + pow( y, 2 ) + pow( z, 2 ) + pow( w, 2 ) );
+	}
 
-	Quaternion& normalize();
+	Quaternion& normalize()
+	{
+		const double norm = getNorm();
+		x /= norm;
+		y /= norm;
+		z /= norm;
+		z /= norm;
+		assert( isNormalized() );
+		return *this;
+	}
 
-	bool isNormalized() const;
+	bool isNormalized() const { return Tolerances::isEqualAsDenominator( getNorm(), 1.0 ); }
 
 private:
 	double x;
