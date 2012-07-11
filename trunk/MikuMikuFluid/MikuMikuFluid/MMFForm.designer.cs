47760dedcf8243d14cd6de986f23bc6559253606
cryphous@gmail.com <cryphous@gmail.com@c4768a9b-8396-9c1e-e381-6b4412480993>
2012-07-11 11:08:14 +0000

    MikuMikuFluid開発を再開．

---
+++ trunk/MikuMikuFluid/MikuMikuFluid/MMFForm.designer.cs
@@ -0,0 +1,225 @@
+﻿namespace MikuMikuFluid
+{
+    partial class MMFForm
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.settingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.objectSettingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.graphicsSettingGToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listBoxInformation = new System.Windows.Forms.ListBox();
+            this.buttonRefresh = new System.Windows.Forms.Button();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.timerSimulation = new System.Windows.Forms.Timer(this.components);
+            this.buttonNextStep = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.buttonViewReset = new System.Windows.Forms.Button();
+            this.buttonParticleInfo = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.settingToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(740, 26);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // settingToolStripMenuItem
+            // 
+            this.settingToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.objectSettingToolStripMenuItem,
+            this.graphicsSettingGToolStripMenuItem});
+            this.settingToolStripMenuItem.Name = "settingToolStripMenuItem";
+            this.settingToolStripMenuItem.Size = new System.Drawing.Size(86, 22);
+            this.settingToolStripMenuItem.Text = "Settings(&S)";
+            // 
+            // objectSettingToolStripMenuItem
+            // 
+            this.objectSettingToolStripMenuItem.Name = "objectSettingToolStripMenuItem";
+            this.objectSettingToolStripMenuItem.Size = new System.Drawing.Size(187, 22);
+            this.objectSettingToolStripMenuItem.Text = "ObjectSetting(&O)";
+            this.objectSettingToolStripMenuItem.Click += new System.EventHandler(this.objectSettingToolStripMenuItem_Click);
+            // 
+            // graphicsSettingGToolStripMenuItem
+            // 
+            this.graphicsSettingGToolStripMenuItem.Name = "graphicsSettingGToolStripMenuItem";
+            this.graphicsSettingGToolStripMenuItem.Size = new System.Drawing.Size(187, 22);
+            this.graphicsSettingGToolStripMenuItem.Text = "GraphicsSetting(&G)";
+            this.graphicsSettingGToolStripMenuItem.Click += new System.EventHandler(this.graphicsSettingGToolStripMenuItem_Click);
+            // 
+            // listBoxInformation
+            // 
+            this.listBoxInformation.FormattingEnabled = true;
+            this.listBoxInformation.ItemHeight = 12;
+            this.listBoxInformation.Location = new System.Drawing.Point(12, 12);
+            this.listBoxInformation.Name = "listBoxInformation";
+            this.listBoxInformation.Size = new System.Drawing.Size(136, 256);
+            this.listBoxInformation.TabIndex = 4;
+            // 
+            // buttonRefresh
+            // 
+            this.buttonRefresh.Location = new System.Drawing.Point(15, 338);
+            this.buttonRefresh.Name = "buttonRefresh";
+            this.buttonRefresh.Size = new System.Drawing.Size(136, 25);
+            this.buttonRefresh.TabIndex = 2;
+            this.buttonRefresh.Text = "Refresh";
+            this.buttonRefresh.UseVisualStyleBackColor = true;
+            this.buttonRefresh.Click += new System.EventHandler(this.buttonRefresh_Click);
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(15, 369);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(136, 23);
+            this.buttonStart.TabIndex = 1;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // timerSimulation
+            // 
+            this.timerSimulation.Interval = 1;
+            this.timerSimulation.Tick += new System.EventHandler(this.timerSimulation_Tick);
+            // 
+            // buttonNextStep
+            // 
+            this.buttonNextStep.Location = new System.Drawing.Point(15, 398);
+            this.buttonNextStep.Name = "buttonNextStep";
+            this.buttonNextStep.Size = new System.Drawing.Size(136, 23);
+            this.buttonNextStep.TabIndex = 5;
+            this.buttonNextStep.Text = "NestStep";
+            this.buttonNextStep.UseVisualStyleBackColor = true;
+            this.buttonNextStep.Click += new System.EventHandler(this.buttonNextStep_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(585, 448);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 26);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.pictureBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.buttonParticleInfo);
+            this.splitContainer1.Panel2.Controls.Add(this.buttonViewReset);
+            this.splitContainer1.Panel2.Controls.Add(this.listBoxInformation);
+            this.splitContainer1.Panel2.Controls.Add(this.buttonRefresh);
+            this.splitContainer1.Panel2.Controls.Add(this.buttonStart);
+            this.splitContainer1.Panel2.Controls.Add(this.buttonNextStep);
+            this.splitContainer1.Size = new System.Drawing.Size(740, 448);
+            this.splitContainer1.SplitterDistance = 585;
+            this.splitContainer1.TabIndex = 7;
+            // 
+            // buttonViewReset
+            // 
+            this.buttonViewReset.Location = new System.Drawing.Point(15, 297);
+            this.buttonViewReset.Name = "buttonViewReset";
+            this.buttonViewReset.Size = new System.Drawing.Size(136, 25);
+            this.buttonViewReset.TabIndex = 6;
+            this.buttonViewReset.Text = "ViewReset";
+            this.buttonViewReset.UseVisualStyleBackColor = true;
+            this.buttonViewReset.Click += new System.EventHandler(this.buttonViewReset_Click);
+            // 
+            // buttonParticleInfo
+            // 
+            this.buttonParticleInfo.Location = new System.Drawing.Point(12, 243);
+            this.buttonParticleInfo.Name = "buttonParticleInfo";
+            this.buttonParticleInfo.Size = new System.Drawing.Size(136, 25);
+            this.buttonParticleInfo.TabIndex = 7;
+            this.buttonParticleInfo.Text = "ParticleInfo";
+            this.buttonParticleInfo.UseVisualStyleBackColor = true;
+            this.buttonParticleInfo.Click += new System.EventHandler(this.buttonParticleInfo_Click);
+            // 
+            // MMFForm
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(740, 474);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.menuStrip1);
+            this.KeyPreview = true;
+            this.Name = "MMFForm";
+            this.Text = "MainForm";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MainForm_KeyDown);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem settingToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem objectSettingToolStripMenuItem;
+        private System.Windows.Forms.ListBox listBoxInformation;
+        private System.Windows.Forms.Button buttonRefresh;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Timer timerSimulation;
+        private System.Windows.Forms.Button buttonNextStep;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button buttonViewReset;
+        private System.Windows.Forms.ToolStripMenuItem graphicsSettingGToolStripMenuItem;
+        private System.Windows.Forms.Button buttonParticleInfo;
+    }
+}
---
+++ trunk/MikuMikuFluid/MikuMikuFluid/SimulationSettingDialog.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Crystal.Command;
+
+using ManagedPosition = System.Collections.Generic.List<double>;
+
+namespace Cryphous
+{
+    public partial class SimulationSettingDialog : Form
+    {
+        private ObjectSettingCommand command;
+        private BoundarySettingCommand bsCommand;
+        private SimulationSettingCommand ssCommand;
+        private Random rand = new Random();
+        List<ManagedPosition> initialPositions;
+        
+        public SimulationSettingDialog(ObjectSettingCommand command, BoundarySettingCommand bsCommand, SimulationSettingCommand ssCommand, List<ManagedPosition> initialPositions)
+        {
+            InitializeComponent();
+            this.command = command;
+            this.bsCommand = bsCommand;
+            this.ssCommand = ssCommand;
+            this.initialPositions = initialPositions;
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void PhysicsObjectSettingDialog_Load(object sender, EventArgs e)
+        {
+            bsCommand.displayBoundarySetting(dataGridView1);
+            ssCommand.setTextBox(textBoxTimeStep, textBoxEffectLength);
+            
+            buttonAdd_Click(sender, e);
+        }
+
+        private void dataGridViewObjectSetting_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            dataGridViewObjectSetting.Rows.Add("Fluid", 100000.0, 20.0, 1000.0, -1.0, 1.0, 0.0, 1.0, -1.0, 1.0, "Box");
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            foreach(DataGridViewRow row in dataGridViewObjectSetting.SelectedRows ) {
+                dataGridViewObjectSetting.Rows.Remove( row );
+            }
+        }
+
+        private void buttonOK_Click(object sender, EventArgs e)
+        {
+            bsCommand.saveBoundarySetting(dataGridView1);
+            ssCommand.save();
+            command.refresh();
+
+            System.Diagnostics.Debug.Assert( dataGridViewObjectSetting.Rows.Count == 1);
+            foreach (DataGridViewRow row in dataGridViewObjectSetting.Rows)
+            {
+                command.saveSettings(row.Cells[0].Value.ToString(), Convert.ToDouble(row.Cells[3].Value), Convert.ToDouble(row.Cells[1].Value), Convert.ToDouble(row.Cells[2].Value), initialPositions);
+            }
+            
+            Hide();
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            Hide();
+        }
+
+        private void dataGridView1_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void PhysicsObjectSettingDialog_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true;
+            this.Hide();
+        }
+
+        private void setBoundary(double minX, double maxX, double minY, double maxY, double minZ, double maxZ)
+        {
+            dataGridView1.Rows[0].Cells[0].Value = minX;
+            dataGridView1.Rows[0].Cells[1].Value = minY;
+            dataGridView1.Rows[0].Cells[2].Value = minZ;
+            dataGridView1.Rows[1].Cells[0].Value = maxX;
+            dataGridView1.Rows[1].Cells[1].Value = maxY;
+            dataGridView1.Rows[1].Cells[2].Value = maxZ;
+        }
+    }
+}
