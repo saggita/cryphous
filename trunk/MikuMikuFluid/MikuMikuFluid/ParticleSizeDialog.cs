3209092433dda13523432f7becf582135638c46c
cryphous@gmail.com <cryphous@gmail.com@c4768a9b-8396-9c1e-e381-6b4412480993>
2012-08-23 13:33:53 +0000

    モデルのボーンから，流体への作用を実装．

--- trunk/MikuMikuFluid/MikuMikuFluid/ParticleSizeDialog.cs
+++ trunk/MikuMikuFluid/MikuMikuFluid/ParticleSizeDialog.cs
@@ -11,6 +11,8 @@ namespace MikuMikuFluid__
 {
     public partial class ParticleSizeDialog : Form
     {
+        private Bitmap textureImage;
+
         public ParticleSizeDialog()
         {
             InitializeComponent();
@@ -26,10 +28,41 @@ namespace MikuMikuFluid__
             get { return (float)numericUpDownY.Value; }
         }
 
+        public Bitmap TextureImage
+        {
+            get { return textureImage; }
+        }
+
         private void ParticleSizeDialog_Load(object sender, EventArgs e)
         {
             numericUpDownX.DecimalPlaces = 1;
             numericUpDownY.DecimalPlaces = 1;
+            numericUpDownAlphaScale.DecimalPlaces = 1;
+        }
+
+        private void buttonTexture_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Image Files|*.bmp;*.png;*.jpg;*.gif|All Files|*.*";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                textureImage = new Bitmap(dialog.FileName);
+                for (int x = 0; x < textureImage.Width; ++x)
+                {
+                    for (int y = 0; y < textureImage.Height; ++y)
+                    {
+                        Color color = textureImage.GetPixel(x, y);
+                        textureImage.SetPixel(x, y, getColorWithAlpha(color));
+                    }
+                }
+            }
+        }
+
+        private Color getColorWithAlpha(Color color)
+        {
+            double alpha = 0.299 * color.R + 0.587 * color.G + 0.114 * color.B;
+            alpha *= (double)numericUpDownAlphaScale.Value;
+            return Color.FromArgb((int)alpha, color.R, color.G, color.B);
         }
     }
 }
