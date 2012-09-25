4da7ce3729a0353cba0aae08ebcbd3e2cc9a63b8
cryphous@gmail.com <cryphous@gmail.com@c4768a9b-8396-9c1e-e381-6b4412480993>
2012-09-25 13:02:58 +0000

    レンダリング用行列クラスを追加． 単体テストは未整備．

--- Crystal/CrystalRenderer/PointSpriteRendererTest.cpp
+++ Crystal/CrystalRenderer/PointSpriteRendererTest.cpp
@@ -3,7 +3,6 @@
 
 #include "../CrystalRenderer/PointSpriteRenderer.h"
 
-#include "Bitmap.h"
 
 using namespace Crystal::Renderer;
 
@@ -12,6 +11,6 @@ BOOST_AUTO_TEST_CASE( POINT_SPRITE_RENDERER_TEST )
 	FrameBuffer buffer(256, 256);
 	PointSprite sprite(5, Color4d(0.0, 0.0, 1.0));
 	PointSpriteRenderer renderer( buffer);
-	renderer.render( sprite, Point2d(10,10));
+	renderer.renderSurface( sprite, Point25d( Point2d(10,10), 0.5f));
 	BOOST_CHECK( buffer.getColor(Point2d(10,10)) == Color4d(0.0, 0.0,1.0) );
 }
