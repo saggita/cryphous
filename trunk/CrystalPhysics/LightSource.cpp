743c8aec7ad9a3aaf2b9664dd9dd58224ed8659f
cryphous@gmail.com <cryphous@gmail.com@c4768a9b-8396-9c1e-e381-6b4412480993>
2012-10-17 12:49:38 +0000

    フォトンの乱数生成に C++11　の random_device を導入． VS2012　が対応している C++11　の標準規格は積極的に試す予定．

--- trunk/CrystalPhysics/LightSource.cpp
+++ trunk/CrystalPhysics/LightSource.cpp
@@ -1,5 +1,7 @@
 #include "LightSource.h"
 
+#include <random>
+
 using namespace Crystal::Physics;
 
 #ifdef _OPENMP
@@ -8,13 +10,14 @@ using namespace Crystal::Physics;
 
 void LightSource::integrateTime(const float proceedTime)
 {
+	std::tr1::random_device rand;
 	if( getPhotons().size() < 500000 ) {
 		std::vector<Geom::Vector3d> points;
 		std::vector<Geom::Vector3d> velocities;
 		
 		for( int i = 0; i < 100; ++i ) {
 			points.push_back( center );
-			velocities.push_back( Geom::Vector3d( (rand() % 1000) / 1000.0f, (rand() % 1000) / 1000.0f, (rand() % 1000) / 1000.0f ) );
+			velocities.push_back( Geom::Vector3d( rand(), rand(), rand() ) );
 			velocities.back().normalize();
 			velocities.back() *= 10.0;
 		}
