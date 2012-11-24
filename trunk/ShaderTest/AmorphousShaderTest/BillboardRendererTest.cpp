cdb4e145c0eb45278dbc22cf7d8327212fa5fd0a
cryphous@gmail.com <cryphous@gmail.com@c4768a9b-8396-9c1e-e381-6b4412480993>
2012-11-24 11:44:06 +0000

    BillboardRenderer を汎用性を高めるように改良． ScreenSpaceFluidRendering ではビルボードレンダリングを多用する．

---
+++ trunk/ShaderTest/AmorphousShaderTest/BillboardRendererTest.cpp
@@ -0,0 +1,67 @@
+#include "StdAfx.h"
+#include "BillboardRendererTest.h"
+
+using namespace Amorphous::Geom;
+using namespace Amorphous::Color;
+using namespace Amorphous::Shader;
+
+const float size = 10.0f;
+const float alpha = 1.0f;
+
+BillboardRendererTest::BillboardRendererTest(const int width, const int height) :
+OnScreenRendererBase( width, height)
+{
+	billboardRenderer = new BillboardRenderer( getWidth(), getHeight(), size, alpha);
+	visualParticles.push_back( VisualParticle() );
+	billboardRenderer->setVisualParticles( visualParticles );
+}
+
+BillboardRendererTest::~BillboardRendererTest(void)
+{
+	delete billboardRenderer;
+}
+
+void BillboardRendererTest::onRender()
+{
+	billboardRenderer->render( *frameBufferObject );
+
+	glClearColor( 0.0f, 0.0f, 0.0f, 0.0f );
+    glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+	glEnable( GL_DEPTH_TEST );
+
+	TextureObject& textureObject = frameBufferObject->getTextureObject();
+	textureObject.apply( 0 );
+
+	GLfloat colorOffset[3] = {0.0f, 0.0f, 0.0f};
+
+	std::vector<double> points(12);
+	points[0] = 0.0; points[1] = 1.0; points[2] = 0.0;
+	points[3] = 0.0; points[4] = 0.0; points[5] = 0.0;
+	points[6] = 1.0; points[7] = 0.0; points[8] = 0.0;
+	points[9] = 1.0; points[10] = 1.0; points[11] = 0.0;
+	
+	shaderObject.apply();
+	shaderObject.setUniformMatrix("projectionMatrix", projectionMatrix);
+	shaderObject.setUniformMatrix("modelviewMatrix", GLSLMatrix());
+	shaderObject.setUniformTexture("offScreenTexture", textureObject);
+	shaderObject.setUniformVector("colorOffset", colorOffset);
+	shaderObject.setUniform("intensityScale", 1.0f);
+	shaderObject.setVertex("position", points );
+	shaderObject.drawQuads( 4);
+	shaderObject.release();
+
+	textureObject.release();
+
+	glDisable( GL_DEPTH_TEST );
+}
+
+void BillboardRendererTest::onInit()
+{
+	frameBufferObject.reset( new FrameBufferObject(getWidth(), getHeight(), false) );
+
+	billboardRenderer->init();
+
+	shaderObject.createShader("IntensityOffsetter");
+
+	projectionMatrix.setOrthogonalMatrix( 0.0, 1.0, 0.0, 1.0, -1.0, 1.0 );
+}
