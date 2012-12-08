82ea0b271ff443846f4688404d137c29025d77e8
cryphous@gmail.com <cryphous@gmail.com@c4768a9b-8396-9c1e-e381-6b4412480993>
2012-12-08 08:08:06 +0000

    ポイントスプライトをピクセル単位でシェーディングできるように改良．

--- ShaderTest/Shaders/PointSprite.fs
+++ ShaderTest/Shaders/PointSprite.fs
@@ -3,13 +3,20 @@
 in vec4 color;
 out vec4 fragColor;
 
+
 void main(void)
 {
-	vec3 N;
-	N.xy = gl_PointCoord.xy * vec2(2.0, -2.0) + vec2(-1.0, 1.0);
-	float distance = dot(N.xy, N.xy);
-	if (distance > 1.0)
+	vec3 normal;
+	normal.xy = gl_PointCoord.xy * vec2(2.0, -2.0) + vec2(-1.0, 1.0);
+	float distance = dot(normal.xy, normal.xy);
+	if (distance > 1.0) {
 		discard;
-	fragColor.rgb = color.rgb;
-	fragColor.a = 0.5;//color.a * texture(alphaTexture, gl_PointCoord.xy).a;
+	}
+	normal.z = sqrt( 1.0 - distance );
+	vec3 lightDirection = vec3(1.0, 1.0, 1.0);
+	normalize( lightDirection );
+
+	float diffuse = max( 0.0, dot(lightDirection, normal));
+	fragColor.rgb = color.rgb * diffuse;
+	fragColor.a = 0.5;
 }
